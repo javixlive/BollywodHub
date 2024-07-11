2fe533d40691acd9c91761e1aec3cde184feb7c0
javixlive <javixlive@hotmail.com>
2024-07-11 13:34:43 -0600

    added api

--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -1,10 +1,27 @@
 import { Injectable, inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
+const token = process.env.TOKEN;
+
+const options = {
+    include_adult: 'false',
+    include_video: 'true',
+    page: '1',
+    headers: {
+        accept: 'application/json',
+        Authorization: token
+    }
+}
+
 @Injectable({
     providedIn: 'root'
 })
 
 export class MovieService {
+    
     http = inject(HttpClient);
+
+    getMovies() {
+        return this.http.get<any>('https://api.themoviedb.org/3/movie/', options)
+    }
 }
