8a52a997e565f5af7e373e0ce471ce2e103879e4
javixlive <javixlive@hotmail.com>
2024-07-13 19:14:35 -0600

    sign up/login working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { MovieCardDetailsComponent } from './components/movie-card-details/movie-card-details.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {path: 'home', component:HomeComponent},
   {path: 'movie/:id', component:MovieCardDetailsComponent},
   {path: 'search/:text', component:SearchComponent},
+  {path: 'auth', component:AuthComponent},
+  
 
   {path: '', pathMatch:'full', redirectTo:'/home'},
   {path: '**',redirectTo:'/home'}
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,16 +14,22 @@ import { MovieCardDetailsComponent } from './components/movie-card-details/movie
 import { HttpClientModule } from '@angular/common/http';
 import { SearchComponent } from './components/search/search.component';
 import { ScrollTopComponent } from './components/scroll-top/scroll-top.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     HeaderComponent,
     HomeComponent,
     MovieCardComponent,
@@ -30,7 +37,7 @@ import { ScrollTopComponent } from './components/scroll-top/scroll-top.component
     AppRoutingModule,
     ScrollTopComponent,
     SearchComponent,
-    MovieCardDetailsComponent
+    MovieCardDetailsComponent,
   ],
   providers: [
     provideClientHydration()
