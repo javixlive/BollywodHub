cbc5891a862f039673879551b66fda456cbd61b3
javixlive <javixlive@hotmail.com>
2024-07-14 02:11:41 -0600

    styles

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from "./auth.service";
 @Component ({
     selector: 'app-auth',
     templateUrl: 'auth.component.html',
+    styleUrl: 'auth.component.scss'
 })
 
 export class AuthComponent {
